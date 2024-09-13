904e0d3031ea99626b1708ea6fe3c4f001b7743c
Emerson Dias <emerson_cdias@hotmail.com>
2024-09-12 19:33:21 -0600

    Add Chart

---
+++ app/(root)/page.tsx
@@ -0,0 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import React from 'react'
+
+const Home = () => {
+  const loggedIn = {
+    firstName: 'Emerson',
+  }
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-content">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage you account your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
