df6e32efc1db6c3e1b20fdd07ed1a28061d433bb
Emerson Dias <emerson_cdias@hotmail.com>
2024-09-17 16:17:26 -0600

    chore(.env and sentry): remove .env from git tracking and add sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://5166e6087a358239ae08eaceace2728a@o317982.ingest.us.sentry.io/4507970493546496',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
